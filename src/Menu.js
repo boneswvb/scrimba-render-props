a8908c5504b61bde2ab7dbb4a0b224fc56ab983e
Wim von Benecke <boneswvb@gmail.com>
2020-01-22 03:26:44 +0200

    final load

---
+++ src/Menu.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Toggler from "./Toggler"
+
+function Menu(props) {
+  return (
+    <Toggler defaultOnValue={ true } render={({on, toggle}) => (
+      <div>
+        <button onClick={ toggle }>{ on ? "Hide" : "Show" } Menu </button>
+        <nav style={{ display: on ? "block" : "none" }}>
+            <h6>Signed in as Coder123</h6>
+            <p>Your Profile</p>
+            <p>Your Repositories</p>
+            <p>Your Stars</p>
+            <p>Your Gists</p>
+        </nav>
+      </div>
+    )} />
+  ) 
+}
+
+export default Menu;
---
+++ src/Toggler.js
@@ -0,0 +1,31 @@
+import React, {Component} from "react"
+// HOC is a function that takes a component as a parameter
+// and returns a new component wrapping the given component
+// and "supercharging" it by giving it some extra abilities
+
+class Toggler extends Component {
+  state = {
+    on: this.props.defaultOnValue
+  }
+    
+    toggle = () => {
+      this.setState(prevState => {
+        return {
+            on: !prevState.on
+        }
+      })
+    }
+    
+  render() {
+    return (
+      <div>
+        { this.props.render({
+          on: this.state.on,
+          toggle: this.toggle
+        }) }
+      </div>  
+    )
+  }
+}
+
+export default Toggler;
